9eca745f07bf36484651fab87006037860a22a50
lauraschr <laura.schroeder1@rwth-aachen.de>
2018-12-04 14:47:11 +0100

    Feedback eingearbeitet

--- AnalyseScript.R
+++ AnalyseScript.R
@@ -1,13 +1,13 @@
 # Analyse Script ----
 
-install.packages("tidyverse")
-install.packages("lubridate")
-install.packages("psych")
-install.packages("esquisse")
-install.packages("ggthemes")
-install.packages("ggplot2")
+#install.packages("tidyverse")
+#install.packages("lubridate")
+#install.packages("psych")
+#install.packages("esquisse")
+#install.packages("ggthemes")
+#install.packages("ggplot2")
 
-install.packages("devtools")
+#install.packages("devtools")
 library(devtools)
 devtools::install_github("HCIC/r-tools")
 
@@ -43,6 +43,17 @@ raw.short$gender <- as.factor(raw.short$gender)
 raw.short$job <- as.factor(raw.short$job)
 raw.short$socme <- as.factor(raw.short$socme)
 
+scale.education <- c("Hauptschulabschluss",
+                     "Realschulabschluss",
+                     "Fachabitur/Abitur",
+                     "Berufsausbildung",
+                     "Studienabschluss (Bachelor, Master, Magister, Diplom, Promotion etc.)")
+
+scale.residence <- c("Ich wohne auf dem Land.",
+                     "Ich wohne im Vorort einer Stadt.",
+                     "Ich wohne am Stadtrand.",
+                     "Ich wohne zentral in einer Stadt.")
+
 
 
 scale.zustimmung <- c("Stimme gar nicht zu",
@@ -62,6 +73,10 @@ scale.zustimmung2 <- c("stimme gar nicht zu",
 
 
 # kut1-8 = = scale.zustimmung
+raw.short$edu <- ordered(raw.short$edu, levels = scale.education)
+
+raw.short$residence <- ordered(raw.short$residence, levels = scale.residence)
+
 raw.short$kut1 <- ordered(raw.short$kut1, levels = scale.zustimmung)
 raw.short$kut2 <- ordered(raw.short$kut2, levels = scale.zustimmung)
 raw.short$kut3 <- ordered(raw.short$kut3, levels = scale.zustimmung)
@@ -113,6 +128,10 @@ keyslist <- list (KUT= c("kut1", "-kut2", "kut3", "kut4", "-kut5", "kut6", "-kut
                   DPERSO = c("dperso1", "-dperso2", "dperso3", "-dperso4", "-dperso5"),
                   DSAVE = c("dsave1", "dsave2", "-dsave3"),
                   DTYPE = c("dtype1", "-dtype2", "dtype3"))
+
+keyslist.edu <- list(EDU = c("edu"))
+
+keyslist.residence <- list(RESIDENCE = c("residence"))
                   
         
                   
@@ -120,6 +139,9 @@ keyslist <- list (KUT= c("kut1", "-kut2", "kut3", "kut4", "-kut5", "kut6", "-kut
 #print("Hier werden später Skalen berechnet. Thema am 09.11.2018")
 
 scores <- scoreItems(keyslist, raw.short, missing = TRUE, min = 1, max = 6)
+scores.edu <- scoreItems (keyslist.edu, raw.short, missing = TRUE, min = 1, max = 5)
+scores.residence <- scoreItems (keyslist.residence, raw.short, missing = TRUE, min = 1, max = 4)
+
 
 data <- bind_cols(raw.short, as.tibble(scores$scores))
 data <- data %>% 
@@ -141,9 +163,9 @@ saveRDS(data, "data/smart_identification.rds")
 
 
 # Analyse ----
-print("Hier werden später statistische Analysen durchgeführt. Thema ab dem 16.11.2018")
+#print("Hier werden später statistische Analysen durchgeführt. Thema ab dem 16.11.2018")
 
 # Graphik erstellung ---- 
-print("Hier werden später Grafiken erstellt. Thema ab dem 16.11.2018")
+#print("Hier werden später Grafiken erstellt. Thema ab dem 16.11.2018")
 
 
