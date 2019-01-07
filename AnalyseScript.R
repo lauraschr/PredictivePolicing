65c19fc25f22831e30b477b55a97dc4a84ba5078
alexaotte <alexa.otte@rwth-aachen.de>
2019-01-07 20:47:28 +0100

    blbla

--- AnalyseScript.R
+++ AnalyseScript.R
@@ -1,14 +1,14 @@
-# Analyse Script
-print("Hier werden Pakete eingebunden")
+#Analyse Script
+print ("Hier werden Pakete eingebunden")
 
-# Data Cleaning ----
-print("Hier wird der Datensatz aufbereitet. Thema am 09.11.2018")
+#Data Cleaning ---
+print ("Hier wird der Datensatz aufbereitet. Thema am 09.11.2018")
 
-# Skalenberechnung ----
-print("Hier werden später Skalen berechnet. Thema am 09.11.2018")
+#Skalenberechnung ---
+print ("Hier werden später Skalen berechnet. Thema am 09.11.2018")
 
-# Analyse ----
-print("Hier werden später statistische Analysen durchgeführt. Thema ab dem 16.11.2018")
+#Analyse ---
+print ("Hier werden später statistische Analysen durchgeführt. Thema ab dem 16.11.2018")
 
-# Graphik erstellung ---- 
-print("Hier werden später Grafiken erstellt. Thema ab dem 16.11.2018")
+#Graphik erstellung ---
+print ("Hier werden später Grafiken erstellt. Thema ab dem 16.11.2018")
